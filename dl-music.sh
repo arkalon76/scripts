999ea4242e456acd39a7b7768ca114fb9799b90a
Kenth Fagerlund <kenth.fagerlund@gmail.com>
2019-10-03 10:34:16 +0800

    Initial commit

---
+++ dl-music.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# +-----------------------------+
+# |				|
+# | YouTube music downloader	|
+# |				|
+# | @Kenth Fagerlund 2019	|
+# |				|
+# +-----------------------------+
+
+youtube-dl -x --audio-format vorbis $1
